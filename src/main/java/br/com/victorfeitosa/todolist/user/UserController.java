6bc476d75083f24414555902466d6376eacc0a7c
Victor Feitosa <victor.s.feitosa2@hotmail.com>
2026-01-04 13:53:53 -0300

    Comentario que difere @Controller do @RestController

--- src/main/java/br/com/victorfeitosa/todolist/user/UserController.java
+++ src/main/java/br/com/victorfeitosa/todolist/user/UserController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import at.favre.lib.crypto.bcrypt.BCrypt;
 
+// @Controller Quando precisamos enviar algum template, ex: HTML
 @RestController
 @RequestMapping("/users")
 public class UserController {
